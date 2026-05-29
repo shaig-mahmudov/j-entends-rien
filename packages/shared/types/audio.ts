71205df2d1d4880869e7c509d802ddb9febc14c2
shaig-mahmudov <shaigfm-apa101@code.edu.az>
2026-05-29 15:50:20 +0400

    Add shared audio analysis types

---
+++ packages/shared/types/audio.ts
@@ -0,0 +1,27 @@
+export type EnergyPoint = {
+  time: number;
+  value: number;
+};
+
+export type FrequencyBandPoint = {
+  time: number;
+  bass: number;
+  mid: number;
+  treble: number;
+};
+
+export type AudioSection = {
+  start: number;
+  end: number;
+  type: "intro" | "verse" | "chorus_or_drop" | "bridge" | "outro";
+  intensity: number;
+};
+
+export type AudioAnalysis = {
+  bpm: number;
+  duration: number;
+  beats: number[];
+  energyCurve: EnergyPoint[];
+  frequencyBands: FrequencyBandPoint[];
+  sections: AudioSection[];
+};
